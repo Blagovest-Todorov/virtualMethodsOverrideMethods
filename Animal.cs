91e00f311be0b8cea5518c90eb2c6cb4a59170e7
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-05-17 14:18:04 +0300

    add 5 files

---
+++ Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Animal
+    {
+        public virtual void Eat()   //We declare teh method with keyword virtual, then we can override it
+        {
+            Console.WriteLine("eating…");
+        }
+    }
+}
---
+++ Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Dog : Animal
+    {
+        public void Bark() 
+        {
+            Console.WriteLine("barking…");
+            
+        }
+
+        public override void Eat() //here the virtual method can be overriden, rewritten, to do else
+        {
+            Console.WriteLine("i am eating very slowly");
+        }
+    }
+}
